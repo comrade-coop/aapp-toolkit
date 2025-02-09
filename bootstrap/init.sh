bd7e515404656884fb65cf1778e31e6ba8b0a099
Branimir Angelov <55453003+branimirangelov@users.noreply.github.com>
2025-02-09 18:17:22 +0200

    feat: move to json manifest to reduce deps

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-# Step 1: Download Base64 encoded YAML file
 USER_DATA_BASE64=$(curl -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance/compute/userData?api-version=2021-01-01&format=text")
+USER_DATA_JSON=$(echo "$USER_DATA_BASE64" | base64 --decode)
+DNS_ROOT=$(echo "$USER_DATA_JSON" | jq -r '.spec.ingress.hostname')
 
-# Step 2: Decode the YAML file
-USER_DATA_YAML=$(echo "$USER_DATA_BASE64" | base64 --decode)
-
-# Step 3: Extract the value of spec.ingress.hostname using yq (install it if necessary)
-DNS_ROOT=$(echo "$USER_DATA_YAML" | yq eval '.spec.ingress.hostname' -)
-
-# Step 4: Create CSR for the extracted domain
 OPENSSL_CONF="openssl.cnf"
 CSR_FILE="${DNS_ROOT}.csr"
 KEY_FILE="${DNS_ROOT}.key"
@@ -33,10 +27,9 @@ EOF
 
 openssl req -new -newkey rsa:2048 -nodes -keyout "$KEY_FILE" -out "$CSR_FILE" -config "$OPENSSL_CONF" -subj "/CN=$DNS_ROOT"
 
-# Step 5: Base64 encode CSR and invoke AttestationClient
 CSR_BASE64=$(base64 -w 0 "$CSR_FILE")
 
-cd ../azure-attestation/app
+cd azure-attestation/app
 
 sudo bash pre-requisites.sh
 cmake .
